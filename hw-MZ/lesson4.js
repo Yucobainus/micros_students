f313a0b9bd3d3cfa681d46f4c65ee572204bc552
Mikhail1JS <misha.zaichko@gmail.com>
2022-06-11 21:19:00 +0500

    lesson 4 task

---
+++ hw-MZ/lesson4.js
@@ -0,0 +1,13 @@
+let createObject = (...someArgs) => {    
+    
+
+    return {
+        logins : someArgs.filter(el=> typeof(el) != 'number').join(','),
+        count :  someArgs.filter(el=> typeof(el) == 'number').join(''),
+
+    }
+  
+
+}
+
+console.log(createObject(3,"test","test2"));
