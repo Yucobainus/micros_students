52987d516900e65d8761bc3e882cbabfcff2dae8
Yucobainus <naibrgood@gmail.com>
2022-06-13 18:08:16 +0300

    13062022

---
+++ lessons/13062022/script.js
@@ -0,0 +1,20 @@
+// class User {
+
+//     constructor(login, password) {
+//         login = "User Login: " + login
+//         this.login = login
+//         this.password = password
+//     }
+
+//     showLogin() {
+//         console.log(this.login)
+//     }
+
+// }
+
+// let yura = new User("Yurarara", "12345")
+// yura.password = "abcde"
+// console.log(yura)
+
+// let date = new Date()
+// console.log(Date())
