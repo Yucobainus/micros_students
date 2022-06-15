c47c0307790db50a528ae9683f624005e9baf81b
Yucobainus <naibrgood@gmail.com>
2022-06-15 18:37:48 +0300

    15062022-classwork

--- Ruslan/src/index.js
+++ Ruslan/src/index.js
@@ -1,30 +1,34 @@
-const v1 = document.getElementById("v1");
-const v2 = document.getElementById("v2");
+// const calc = document.getElementById("calc");
 
-console.log(v1);
+// calc.addEventListener("click", () => {
+//   let input1 = document.getElementById("v1")
+//   let num1 = +input1.value
+
+//   let input2 = document.getElementById("v2")
+//   let num2 = +input2.value
+
+//   document.getElementById("result").innerHTML = `result: ${num1 + num2}`;
+// });
 
 let num1 = 0;
 let num2 = 0;
 
-v1.addEventListener("change", (e) => {
-  num1 = +e.target.value;
-  // num1 = Number(e.target.value);
-  // num1 = e.target.value;
+let input1 = document.getElementById("v1")
+let input2 = document.getElementById("v2")
+input1.addEventListener('change', function () {
+  num1 = +input1.value
+})
+input2.addEventListener('change', function () {
+  num2 = +input2.value
+})
+
+let plus = document.getElementById("plus");
+let minus = document.getElementById("minus");
+plus.addEventListener("click", () => {
+  document.getElementById("result").innerHTML = `result: ${num1 + num2}`;
 });
-v2.addEventListener("change", (e) => {
-  num2 = +e.target.value;
-  // num2 = e.target.value;
-});
 
-const res = document.getElementById("calc");
 
-res.addEventListener("click", () => {
-  // console.log(num1, num2);
-  if (num1 && num2) {
-    const sum = num1 + num2;
-
-    document.getElementById("result").innerHTML = `result: ${sum}`;
-    v1.value = "";
-    v2.value = "";
-  }
+minus.addEventListener("click", () => {
+  document.getElementById("result").innerHTML = `result: ${num1 - num2}`;
 });
